f2a9b20796d1dcbdc7fc0d8459a6d750b5cb3e86
marco <makoheuvelman@hotmail.com>
2019-04-19 00:39:49 +0200

    made visuals for statistical distributions

--- statistics.py
+++ statistics.py
@@ -1,34 +1,36 @@
 from scipy import stats
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+BASELINE = 0.745
 
 ESN_errors = []
-
 with open('ESN_100_runs_test_RMSE.csv', newline='') as csvfile:
     daan_reader = csv.reader(csvfile, delimiter=';')
     for row in daan_reader:
-        ESN_errors.append([float(x) for x in row])
+        temp = [float(x) for x in row]
+        ESN_errors.append(temp)
 
-# ESN_errors
-
-# print(ESN_errors)
-
-# rvs = stats.norm.rvs(loc=5, scale=10, size=(50))
-
-# print(rvs)
+ESN_errors = np.array(ESN_errors)
 
 echo_state_N = [1,2,5,10,20,50,100,200]
 
 
-ESN_one_sample = stats.ttest_1samp(ESN_errors, 0.745)
+ESN_one_sample = stats.ttest_1samp(ESN_errors, BASELINE)
 ESN_descriptives = stats.describe(ESN_errors)
 
-for i, N in enumerate(echo_state_N):
-    shapriro_p = stats.shapiro(ESN_errors[:][i])[1]
-    effect_size = ESN_descriptives.mean[i] - 0.745
+fig, subplot_axes = plt.subplots(3, 3, sharey='all', sharex='all')
 
-    print("One sample t-test for ESN. nodes = {:d}, Baseline=0.745: P-Value of {:.3f}, Effect size: {:.3f}".format(
+for i, N in enumerate(echo_state_N):
+
+    shapriro_p = stats.shapiro(ESN_errors[:,i])[1]
+    effect_size = ESN_descriptives.mean[i] - BASELINE
+
+    print("One sample t-test for ESN (nodes = {:d}): Baseline={:.3f}: P-Value of {:.3f}, Effect size: {:.3f}".format(
                 N,
+                BASELINE,
                 ESN_one_sample.pvalue[i],
                 effect_size))
     print("Descriptive statistics: n={:d}, Mean={:.3f}, Variance={:.3f}, Shapiro-P={:.3f}".format(
@@ -37,6 +39,12 @@ for i, N in enumerate(echo_state_N):
                 ESN_descriptives.variance[i],
                 shapriro_p))
     print("\n\n")
+
+    row = i % len(subplot_axes)
+    column = i // len(subplot_axes)
+
+    sns.set(color_codes=True)
+    sns.distplot(ESN_errors[:,i], label='ESN errors nodes = {}'.format(N), ax=subplot_axes[row,column])
 
 
 
@@ -49,16 +57,15 @@ with open('mlp_results.csv', newline='') as csvfile:
             continue
         if (int(row[1]) == 249):
             MLP_errors.append(float(row[-1]))
-        # print(row[2])
 
-# print(MLP_errors)
 
-mlp_one_sample = stats.ttest_1samp(MLP_errors, 0.745)
+mlp_one_sample = stats.ttest_1samp(MLP_errors, BASELINE)
 mlp_descriptives = stats.describe(MLP_errors)
 shapriro_p = stats.shapiro(MLP_errors)[1]
-effect_size = mlp_descriptives.mean - 0.745
+effect_size = mlp_descriptives.mean - BASELINE
 
-print("One sample t-test for ESN. Baseline=0.745: P-Value of {:.3f}, Effect size: {:.3f}".format(
+print("One sample t-test for ESN. Baseline={:.3f}: P-Value of {:.3f}, Effect size: {:.3f}".format(
+            BASELINE,
             mlp_one_sample.pvalue,
             effect_size))
 
@@ -68,11 +75,27 @@ print("Descriptive statistics: n={:d}, Mean={:.3f}, Variance={:.3f}, Shapiro-P={
             mlp_descriptives.variance,
             shapriro_p))
 
+# stats.probplot(MLP_errors, plot=plt)
+sns.distplot(MLP_errors, label='MLP errors',ax = subplot_axes[-1,-1])
+
+# figure = plt.figure(1, clear=True)
+# plt.subplot(331)
 
 
+fig, subplot_axes
+for row in subplot_axes:
+    for axe in row:
+        bot, top = axe.get_ylim()
+        axe.vlines(0.745, bot, top, color='red',linestyles='dashed', label='Baseline')
+        # axe.set_ylabel('Count')
+        axe.set_xlabel('RMSE')
+        axe.legend()
+# axe = plt.gca()
+# bot, top = axe.get_ylim()
+# axe.vlines(0.745, bot, top, color='red',linestyles='dashed', label='Baseline')
+# axe.set_ylabel('Count')
+# axe.set_xlabel('RMSE')
+plt.show()
 
-# mean = np.mean(MLP_errors)
-# variance = np.var(MLP_errors)
-# effect_size = np.mean(MLP_errors)-0.745
 
-# print("Descriptive statistics: Mean {} , Variance {} , Effect size {}".format(mean, variance, effect_size))
+# print(ESN_errors[:,3])
