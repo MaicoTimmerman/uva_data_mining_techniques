3023f5d8b381e312f353f8387fccfcaa24a4cfa2
Unknown <stickfiguredaan@gmail.com>
2019-04-25 17:06:33 +0200

    Add some very basic EDA

---
+++ assignment2/EDA/interrogate_the_dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from data_questions import *
+
+
+if __name__ == "__main__":
+    input_file = "../data/tiny_train.csv"
+    dataset = pd.read_csv(input_file)
+    print('(min, mean, max)')
+    print(f"A search results in {clicks_per_search(dataset)} clicks.")
+    print(f"A search results in {bookings_per_search(dataset)} bookings.")
