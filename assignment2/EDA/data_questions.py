d932d2a40de1c8c1907640b8b2c997dfd3d7b64b
Unknown <stickfiguredaan@gmail.com>
2019-05-03 13:11:32 +0200

    Some more basic EDA

--- assignment2/EDA/data_questions.py
+++ assignment2/EDA/data_questions.py
@@ -2,17 +2,22 @@
 def stats (x):
     return min(x), sum(x)/len(x), max(x)
 
-def clicks_per_search (dataset):
+def count_clicks_per_search (dataset):
     df = dataset[['srch_id', 'click_bool']]
     clicks_per_search = [df2['click_bool'].sum() for srch_id, df2 in df.groupby('srch_id')]
     return stats(clicks_per_search)
 
-def bookings_per_search (dataset):
+def count_bookings_per_search (dataset):
     df = dataset[['srch_id', 'booking_bool']]
     bookings_per_search = [df2['booking_bool'].sum() for srch_id, df2 in df.groupby('srch_id')]
     return stats(bookings_per_search)
 
-def clicks_per_booking (dataset):
+def count_clicks_per_booking (dataset):
     df = dataset[['booking_bool', 'click_bool']]
     clicks_per_booking = df[df['booking_bool'] == True]['click_bool']
+    return stats(clicks_per_booking)
+
+def calc_price_per_booking (dataset):
+    df = dataset[['booking_bool', 'price_usd']]
+    clicks_per_booking = df[df['booking_bool'] == True]['price_usd']
     return stats(clicks_per_booking)
--- assignment2/EDA/interrogate_the_dataset.py
+++ assignment2/EDA/interrogate_the_dataset.py
@@ -5,7 +5,29 @@ from data_questions import *
 if __name__ == "__main__":
     input_file = "../data/tiny_train.csv"
     dataset = pd.read_csv(input_file)
+
+    clicks_per_search = count_clicks_per_search(dataset)
+    bookings_per_search = count_bookings_per_search(dataset)
+    clicks_per_booking = count_clicks_per_booking(dataset)
+    price_per_booking = calc_price_per_booking(dataset)
+
     print('(min, mean, max)')
-    print(f"A search results in {clicks_per_search(dataset)} clicks.")
-    print(f"A search results in {bookings_per_search(dataset)} bookings.")
-    print(f"A booking has {clicks_per_booking(dataset)} clicks.")
+    print(f"A search results in {clicks_per_search} clicks.")
+    print(f"A search results in {bookings_per_search} bookings.")
+    print(f"A booking has {clicks_per_booking} clicks.")
+    print(f"A booking costs {price_per_booking} usd.")
+
+    does_ignored_search_exist = clicks_per_search[0] < 1
+    does_plural_booking_exist = bookings_per_search[2] > 1
+    does_ghost_booking_exist = clicks_per_booking[0] < 1
+    does_negative_price_exist = price_per_booking[0] < 0
+    is_there_a_problem = (
+        does_ignored_search_exist
+        | does_ghost_booking_exist
+        | does_plural_booking_exist
+    )
+    if is_there_a_problem:
+        for _ in range(10):
+            print("UH OH")
+    else:
+        print("Seems okay")
