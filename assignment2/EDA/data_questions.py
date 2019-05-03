7c30dff82645462e4e2b966145ebd07389e83541
Unknown <stickfiguredaan@gmail.com>
2019-05-03 13:56:54 +0200

    More basic EDA

--- assignment2/EDA/data_questions.py
+++ assignment2/EDA/data_questions.py
@@ -21,3 +21,17 @@ def calc_price_per_booking (dataset):
     df = dataset[['booking_bool', 'price_usd']]
     clicks_per_booking = df[df['booking_bool'] == True]['price_usd']
     return stats(clicks_per_booking)
+
+def count_countries_per_search (dataset):
+    df = dataset[['srch_id', 'prop_country_id']]
+    countries_per_search = [len(df2['prop_country_id'].unique()) for srch_id, df2 in df.groupby('srch_id')]
+    return stats(countries_per_search)
+
+def count_hotel_countries (dataset):
+    return len(dataset['prop_country_id'].unique())
+
+def count_visitor_countries (dataset):
+    return len(dataset['visitor_location_country_id'].unique())
+
+def count_destinations (dataset):
+    return len(dataset['srch_destination_id'].unique())
--- assignment2/EDA/interrogate_the_dataset.py
+++ assignment2/EDA/interrogate_the_dataset.py
@@ -10,12 +10,20 @@ if __name__ == "__main__":
     bookings_per_search = count_bookings_per_search(dataset)
     clicks_per_booking = count_clicks_per_booking(dataset)
     price_per_booking = calc_price_per_booking(dataset)
+    countries_per_search = count_countries_per_search(dataset)
+    n_hotel_countries = count_hotel_countries(dataset)
+    n_visitor_countries = count_visitor_countries(dataset)
+    n_destinations = count_destinations(dataset)
 
     print('(min, mean, max)')
     print(f"A search results in {clicks_per_search} clicks.")
     print(f"A search results in {bookings_per_search} bookings.")
     print(f"A booking has {clicks_per_booking} clicks.")
     print(f"A booking costs {price_per_booking} usd.")
+    print(f"A search results in hotels in {countries_per_search} different countries.")
+    print(f"There are {n_hotel_countries} different hotel countries.")
+    print(f"There are {n_visitor_countries} different visitor countries.")
+    print(f"There are {n_destinations} different search destinations.")
 
     does_ignored_search_exist = clicks_per_search[0] < 1
     does_plural_booking_exist = bookings_per_search[2] > 1
