c943eabbf7d027053fe81a45e2a9ea5835a46c45
Unknown <stickfiguredaan@gmail.com>
2019-05-17 14:28:03 +0200

    Find prop_id overlap in test set (99.72%!!)
    This validates our cheat sheet approach

---
+++ assignment2/EDA/find_prop_id_overlap.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def prop_id_overlap (file1, file2):
+    prop_ids_1 = pd.read_csv('../data/'+file1, dtype={'prop_id':int})['prop_id'].unique()
+    prop_ids_2 = pd.read_csv('../data/'+file2, dtype={'prop_id':int})['prop_id']
+    match_count = sum(prop_ids_2.isin(prop_ids_1))
+    return match_count, len(prop_ids_1), len(prop_ids_2)
+
+def print_overlap (set1, set2):
+    matches, len1, len2 = prop_id_overlap(set1, set2)
+    print(f"{matches} out of {len2} from {set2} are present in {set1}. That is an overlap of {100.0*matches/len2:.2f}%.")
+
+print_overlap('tiny_train.csv', 'tiny_test.csv')
+print_overlap('training_set_VU_DM.csv', 'test_set_VU_DM.csv')
+
+# 11562 out of 24754 from tiny_test.csv are present in tiny_train.csv. That is an overlap of 46.71%.
+# 4945334 out of 4959183 from test_set_VU_DM.csv are present in training_set_VU_DM.csv. That is an overlap of 99.72%.
