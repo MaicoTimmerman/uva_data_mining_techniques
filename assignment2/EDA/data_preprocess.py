ec7aaa97a9fd752130f28cbdfb6ddad353e42e7c
Unknown <makoheuvelman@hotmail.com>
2019-05-21 15:23:46 +0200

    new cheat sheets, entire, based on 90

--- assignment2/EDA/data_preprocess.py
+++ assignment2/EDA/data_preprocess.py
@@ -195,12 +195,7 @@ def id_hacking(df):
 
         df = df.merge(cheat_sheet, how='left', left_on=cheat_sheet.index.name, right_on=cheat_sheet.index.name)
 
-        # cheat_temp = ['price_usd']
         for variable_name in cheat_sheet.columns:
-        # for variable_name in cheat_temp:
-            # print(variable_name)
-            # pd.merge(left=survey_sub,right=species_sub, how='left', left_on='species_id', right_on='species_id')
-            # df[variable_name] = cheat_sheet.loc[df.visitor_location_country_id][variable_name].values
             df.fillna({variable_name: 0}, inplace=True)
         print("finished merging {} with dataframe".format(pickle_file))
         return df
